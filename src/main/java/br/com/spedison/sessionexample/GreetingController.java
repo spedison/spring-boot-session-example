3db8564b0312cc4e2048abfed42f076c10bb108f
Edison Ribeiro Araujo <spedison@users.noreply.github.com>
2020-07-30 08:27:18 -0300

    Limpeza e adicionei um cron para limpar as tabelas a cada 2 anos. Assim dá para acompanhar bem o conteúdo que uma pessoa está utilizando.

--- src/main/java/br/com/spedison/sessionexample/GreetingController.java
+++ src/main/java/br/com/spedison/sessionexample/GreetingController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class GreetingController {
