080f430eef8659aa2632270f0f67651f3e9f91be
Edison Ribeiro Araujo <spedison@users.noreply.github.com>
2020-07-30 08:46:40 -0300

    Script de banco

---
+++ scripts/criacao_tabela.sql
@@ -0,0 +1,28 @@
+-- Usando em postgresql no Schema chamado sessao.
+
+DROP TABLE IF EXISTS sessao.SPRING_SESSION CASCADE ;
+CREATE TABLE sessao.SPRING_SESSION (
+                                       PRIMARY_ID CHAR(36) NOT NULL PRIMARY KEY,
+                                       SESSION_ID CHAR(36) NOT NULL,
+                                       CREATION_TIME BIGINT NOT NULL,
+                                       LAST_ACCESS_TIME BIGINT NOT NULL,
+                                       MAX_INACTIVE_INTERVAL INT NOT NULL,
+                                       EXPIRY_TIME BIGINT NOT NULL,
+                                       PRINCIPAL_NAME VARCHAR(100)
+) ;
+
+CREATE UNIQUE INDEX SPRING_SESSION_IX1 ON sessao.SPRING_SESSION (SESSION_ID);
+CREATE INDEX SPRING_SESSION_IX2 ON sessao.SPRING_SESSION (EXPIRY_TIME);
+CREATE INDEX SPRING_SESSION_IX3 ON sessao.SPRING_SESSION (PRINCIPAL_NAME);
+
+
+DROP TABLE IF EXISTS sessao.SPRING_SESSION_ATTRIBUTES CASCADE ;
+
+CREATE TABLE sessao.SPRING_SESSION_ATTRIBUTES (
+                                                  SESSION_PRIMARY_ID CHAR(36) NOT NULL,
+                                                  ATTRIBUTE_NAME VARCHAR(200) NOT NULL,
+                                                  ATTRIBUTE_BYTES BYTEA NOT NULL,
+                                                  CONSTRAINT SPRING_SESSION_ATTRIBUTES_FK FOREIGN KEY (SESSION_PRIMARY_ID) REFERENCES sessao.SPRING_SESSION(PRIMARY_ID) ON DELETE CASCADE
+) ;
+
+ALTER TABLE sessao.SPRING_SESSION_ATTRIBUTES ADD CONSTRAINT SPRING_SESSION_ATTRIBUTES_PK PRIMARY KEY (SESSION_PRIMARY_ID, ATTRIBUTE_NAME);
